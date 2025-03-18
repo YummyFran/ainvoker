103c6cbc12d7769edaade20e6ab47bf2e21da4a1
Yummy Fran Palomares <yumpalomares@gmail.com>
2025-03-18 18:10:55 +0800

    feat(signup): Added Github Signin Method

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,16 @@
 import { Link } from "react-router-dom"
+import { getAuth } from "firebase/auth"
+import { logOut } from "../utils/auth"
 
 const Home = () => {
   return (
     <div className="min-h-screen flex items-center justify-center text-white">
         <Link to='/signup' className="bg-accentGrad px-8 py-4 rounded-lg m-4">Get Started</Link>
+        {
+          getAuth().currentUser ? (
+            <button onClick={() =>logOut()} className="bg-red-300 px-4 py-2 rounded-md">Sign out</button>
+          ) : ''
+        }
     </div>
   )
 }
